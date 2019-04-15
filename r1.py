7c30ca64302b62f0c429b2437d356a2087b94abe
akshatcx <akshat.c2k@gmail.com>
2019-04-15 17:41:57 +0530

    [Addition] - Rule 1
    -Added r1.py which excecutes rule1 (reflexives) and prints the output

--- r1.py
+++ r1.py
@@ -1,40 +1,62 @@
-def r1():
-     ...:     sc=[]
-     ...:     for word in dat.globalWordList:
-     ...:         if word.word in reflexives:
-     ...:             sc.append([word.sentenceNum,word.chunkNum,dat.globalWordList.index(word)])
-     ...:     for pro in sc:
-     ...:         flag=1
-     ...:         s=dat.sentenceList[pro[0]]
-     ...:         node=s.nodeDict[s.chunkList[pro[1]].nodeName]
-     ...:         x=s.nodeDict[node.nodeParent]
-     ...:         while(x.nodeName[:3]!='VGF' and x.nodeName[:2]!='NP'):
-     ...:             x=s.nodeDict[x.nodeParent]
-     ...:         if(x.nodeName[:2]=='NP'):
-     ...:             for child in x.childList:
-     ...:                 if s.nodeDict[child].nodeRelation=='r6' and s.nodeDict[child]!=node:
-     ...:                     flag=0
-     ...:                     for chunk in s.chunkList:
-     ...:                         if chunk.nodeName==child:
-     ...:                             ans.append([dat.globalWordList[pro[2]].word, "".join([dat.globalWordList[i].word for i in chunk.wordNumList])])
-     ...:         if(flag):
-     ...:             while(x.nodeName[:3]!='VGF'):
-     ...:                 x=s.nodeDict[x.nodeParent]
-     ...:                 print x.nodeName
-     ...: 
-     ...:             for child in x.childList:
-     ...:                 if s.nodeDict[child].nodeRelation=='k1' and s.nodeDict[child]!=node:
-     ...:                     flag=0
-     ...:                     for chunk in s.chunkList:
-     ...:                         if chunk.nodeName==child:
-     ...:                             ans.append([dat.globalWordList[pro[2]].word, "".join([dat.globalWordList[i].word for i in chunk.wordNumList])])
-     ...: 
-     ...:             if(flag):
-     ...:                 x=s.nodeDict[x.nodeParent]
-     ...:             if(x.nodeName[:3]=='CCP' and x.nodeRelation=='k1'):
-     ...:                 for child in x.childList:
-     ...:                     if s.nodeDict[child].nodeName[:2]=='NP':
-     ...:                         for chunk in s.chunklist:
-     ...:                             if chunk.nodeName==child:
-     ...:                                 ans.append([dat.globalWordList[pro[2]].word, "".join([dat.globalWordList[i].word for i in chunk.wordNumList])])
+import os
+import pickle
 
+with open('./pronouns/reflexives','rb') as f:
+    reflexives = pickle.load(f)
+
+def r1():
+    sc=[]
+    for word in var.globalWordList:
+        if word.word in reflexives:
+            sc.append([word.sentenceNum, word.chunkNum])
+    for _ in sc:
+        s = var.sentenceList[_[0]]
+        c = s.chunkList[_[1]]
+        n = s.nodeDict[c.nodeName]
+        x = s.nodeDict[n.nodeParent]
+        fans={}
+        fans['sentence'] = _[0]
+        fans['pronoun'] = _[1]
+        fans['referents'] = []
+        flag = 1
+        while(flag):
+            if x.nodeName[:2] == 'NP':
+                for child in x.childList:
+                    if s.nodeDict[child].nodeRelation == 'r6' and s.nodeDict[child].nodeName != n.nodeName:
+                        fans['referents'].append(s.nodeDict[child].chunkNum)
+                        flag = 0
+            elif x.nodeName[:3] == 'VGF':
+                for child in x.childList:
+                    if s.nodeDict[child].nodeRelation == 'k1' and s.nodeDict[child].nodeName != n.nodeName:
+                        fans['referents'].append(s.nodeDict[child].chunkNum)
+                        flag = 0
+            elif x.nodeName[:3] == 'CCP' and x.nodeRelation == 'k1':
+                for child in x.childList:
+                    if s.nodeDict[child].nodeName[:2] == 'NP' and s.nodeDict[child].nodeName != n.nodeName:
+                        fans['referents'].append(s.nodeDict[child].chunkNum)
+                        flag = 0
+            if x.nodeParent == "None":
+                break
+            x = s.nodeDict[x.nodeParent]
+        res.append(fans)
+
+for f in os.listdir("DATA/PROCESSED-DATA/collection/"):
+    res=[]
+    try:
+        ofile=open("DATA/PROCESSED-DATA/collection/"+f,"rb")
+        var=pickle.load(ofile)    
+        r1()
+
+        for entry in res:
+            s = var.sentenceList[entry['sentence']]
+            c = s.chunkList[entry['pronoun']]
+            r = []
+            for i in entry['referents']:
+                r.append(s.chunkList[i])
+
+            cc = "".join([var.globalWordList[i].word for i in c.wordNumList])
+            rr = "".join([var.globalWordList[i].word for i in r[0].wordNumList])
+            print cc + " -> " + rr
+
+    except:
+        continue
